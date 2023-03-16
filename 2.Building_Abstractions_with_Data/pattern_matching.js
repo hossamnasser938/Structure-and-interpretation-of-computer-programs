f50c8e759aab4330cc62240b32980d263bd82793
hossamnasser938 <hossamnasser938@gmail.com>
2023-03-16 15:01:19 +0200

    finish 2.5

--- 2.Building_Abstractions_with_Data/pattern_matching.js
+++ 2.Building_Abstractions_with_Data/pattern_matching.js
@@ -2,20 +2,37 @@ const MATCH_FAILURE = "failed";
 
 const emptyDictionary = () => ({});
 
-const lookup = (dictionary, key) => dictionary[key];
-const insertKeyVal = (dictionary, key, val) => ({ ...dictionary, [key]: val });
+const removeParentethesis = token => {
+  let t = token
+
+  do {
+    if (t.startsWith("(")) {
+      t = t.slice(1)
+    }
+    else if (t.endsWith(")")) {
+      t = t.slice(0, -1)
+    }
+  }
+  while(t.startsWith("(") || t.endsWith(")"))
+
+  return t
+}
+
+const lookup = (dictionary, key) => dictionary[removeParentethesis(key)]
+const insertKeyVal = (dictionary, key, val) => ({ ...dictionary, [removeParentethesis(key)]: removeParentethesis(val) });
 
 const extendDictionary = (dictionary, key, val) => {
   const currenVal = lookup(dictionary, key);
 
   if (currenVal) {
-    if (currenVal === val) return dictionary;
+    if (currenVal === removeParentethesis(val)) return dictionary;
     return MATCH_FAILURE;
   }
 
   return insertKeyVal(dictionary, key, val);
 };
 
+const constructRule = (pattern, skeleton) => ({pattern, skeleton})
 const skeleton = (rule) => parse(rule.skeleton);
 const pattern = (rule) => parse(rule.pattern);
 
@@ -23,22 +40,19 @@ const isCompound = (obj) => Array.isArray(obj);
 const isAtomic = (obj) =>
   !isCompound(obj) && (typeof obj === "string" || typeof obj === "number");
 
-patternVariable = (pat) => pat.variable;
-patternVal = (pat) => pat.val;
+patternVariable = (pat) => pat[1];
+patternVal = (pat) => pat[0];
 
-const isArbitraryExpression = (pat) => patternVal(pat) === "?";
-const isArbitraryVariable = (pat) => patternVal(pat) === "?v";
-const isArbitraryConstant = (pat) => patternVal(pat) === "?c";
+const isArbitraryExpression = (pat) => patternVal(pat).includes("?");
+const isArbitraryVariable = (pat) => patternVal(pat).includes("?v");
+const isArbitraryConstant = (pat) => patternVal(pat).includes("?c");
 
 const isExpVariable = (exp) => typeof exp === "string";
 const isExpConstant = (exp) => typeof exp === "number";
 
-const shouldBeEvaluated = (sk) => {
-  const r = sk.startsWith(":");
-  return r;
-};
+const shouldBeEvaluated = (sk) => sk[0].includes(":")
 
-const evaluationExpression = (sk) => sk.slice(1);
+const evaluationExpression = (sk) => sk[1];
 
 const next = (arr) => arr[0];
 const rest = (arr) => (arr.length === 2 ? arr[1] : arr.slice(1));
@@ -46,6 +60,7 @@ const rest = (arr) => (arr.length === 2 ? arr[1] : arr.slice(1));
 const simplifier = (rules) => (expAsString) => {
   function simplify(exp) {
     const [rule, dictionary] = scanRoles(rules, exp);
+    console.log('simplify rule, dictionary', rule, dictionary)
 
     if (!rule) return exp;
 
@@ -56,17 +71,15 @@ const simplifier = (rules) => (expAsString) => {
   }
 
   const expression = parse(expAsString);
-
-  if (!isCompound(expression)) {
-    return simplify(expression);
-  } else {
-    return expression.map(simplify);
-  }
+  return simplify(expression)
 };
 
 const scanRoles = (rules, exp) => {
   for (let rule of rules) {
-    const dictionary = match(pattern(rule), exp, emptyDictionary());
+    const pat = pattern(rule)
+    const dictionary = match(pat, exp, emptyDictionary());
+    console.log('scanRoles dictionary', dictionary)
+
     if (dictionary === MATCH_FAILURE) continue;
 
     return [rule, dictionary];
@@ -76,23 +89,13 @@ const scanRoles = (rules, exp) => {
 };
 
 const match = (pat, exp, dictionary) => {
+  console.log('match pat, exp, dictionary', pat, exp, dictionary)
   if (dictionary === MATCH_FAILURE) return MATCH_FAILURE;
 
   if (isAtomic(pat)) {
     if (isAtomic(exp)) {
       if (pat === exp) return dictionary;
       return MATCH_FAILURE;
-    }
-    return MATCH_FAILURE;
-  }
-
-  if (isCompound(pat)) {
-    if (isCompound(exp)) {
-      return match(
-        rest(pat),
-        rest(exp),
-        match(next(pat), next(exp), dictionary)
-      );
     }
     return MATCH_FAILURE;
   }
@@ -112,38 +115,49 @@ const match = (pat, exp, dictionary) => {
       return extendDictionary(dictionary, patternVariable(pat), exp);
     return MATCH_FAILURE;
   }
+
+  if (isCompound(pat)) {
+    if (isCompound(exp)) {
+      return match(
+        rest(pat),
+        rest(exp),
+        match(next(pat), next(exp), dictionary)
+      );
+    }
+    return MATCH_FAILURE;
+  }
 };
 
 const isAtomicSkeleton = (sk) =>
-  isAtomic(sk) && (typeof sk === "string" ? !sk.startsWith(":") : true);
+  isAtomic(sk) && (typeof sk === "string" ? !sk.includes(":") : true);
 
 const instantiate = (skeleton, dictionary) => {
   if (isAtomicSkeleton(skeleton)) return skeleton;
 
-  if (isCompound(skeleton)) return skeleton.map(instantiate);
+  if (shouldBeEvaluated(skeleton)) return lookup(dictionary, evaluationExpression(skeleton));
 
-  if (shouldBeEvaluated(skeleton)) {
-    return lookup(dictionary, evaluationExpression(skeleton));
+  if (isCompound(skeleton)) {
+    return skeleton.reduce((acc, item) => acc.concat(instantiate(item, dictionary)), [])
   }
 };
 
 const derivativeRules = [
-  { pattern: "(dd (?c c) (? v))", skeleton: 0 },
+  // { pattern: "(dd (?c c) (? v))", skeleton: 0 },
   { pattern: "(dd (?v v) (? v))", skeleton: 1 },
-  { pattern: "(dd (?v u) (? v))", skeleton: 0 },
+  // { pattern: "(dd (?v u) (? v))", skeleton: 0 },
   {
     pattern: "(dd (+ (? x1) (? x2)) (? v))",
     skeleton: "(+ (dd (: x1) (: v)) (dd (: x2) (: v)))",
   },
-  {
-    pattern: "(dd (* (? x1) (? x2)) (? v))",
-    skeleton: "(+ (* (: x1) (dd (: x2) (: v))) (* (dd (: x1) (: v)) (: x2)))",
-  },
+  // {
+  //   pattern: "(dd (* (? x1) (? x2)) (? v))",
+  //   skeleton: "(+ (* (: x1) (dd (: x2) (: v))) (* (dd (: x1) (: v)) (: x2)))",
+  // },
 ];
 
 const spreadArr = (obj) => (Array.isArray(obj) ? obj : [obj]);
 
-const parse = (tokens) => {
+const parse = (str) => {
   function parseIter(tokens) {
     if (tokens.length === 0) return [];
     if (tokens.length === 1) return tokens[0];
@@ -159,6 +173,7 @@ const parse = (tokens) => {
 
       const rest1 = tokens.slice(1, splitterIndex + 1);
       const rest2 = tokens.slice(splitterIndex + 1);
+
       return [
         [item, ...spreadArr(parseIter(rest1))],
         ...spreadArr(parseIter(rest2)),
@@ -168,7 +183,10 @@ const parse = (tokens) => {
     }
   }
 
-  return parseIter(tokens.split(" "));
+  if (typeof str === 'string') {
+    return parseIter(str.split(" "))[0];
+  }
+  return str
 };
 
 const getSplitterIndex = (tokens) => {
@@ -177,13 +195,22 @@ const getSplitterIndex = (tokens) => {
   for (let i = 0; i < tokens.length; i++) {
     const token = tokens[i];
     if (token.startsWith("(")) stack.push("(");
-    else if (token.endsWith(")")) stack.pop();
+    else if (token.endsWith(")")) {
+      let s = token
+      do {
+        stack.pop();
+        s = s.slice(0, -1)
+      }
+      while(s.endsWith(")"))
+    }
 
     if (stack.length === 0) return i;
   }
+
+  throw new Error('not well formatted string')
 };
 
 const derive = simplifier(derivativeRules);
 
-console.log(derive("(dd (+ x y) x)"));
-// console.log(parse("(dd (+ x y) x)"));
+// console.log(derive("(dd (+ x y) x)"));
+console.log(derive("(dd (+ (dd x x) (dd y x)))"));
